8b0a2ce706f8173afcf74d9a031186ff84e979fb
三司绫濑搓衣板 <69589368+SanSiLL@users.noreply.github.com>
2020-09-12 21:05:31 +0800

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void butler(void);
+int main(void)
+{
+    printf("I will summon the butlerfuction.\n");
+    butler();
+    printf("Yes.Bring me some tea and wruteable DVDs.\n");
+
+    return 0;
+
+}
+void butler(void) 
+{
+    printf("You rang,sir?\n");
+    getchar();
+ }
